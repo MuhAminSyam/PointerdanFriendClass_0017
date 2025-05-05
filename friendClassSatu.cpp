c1219d24aff6bed97cfc2d571e7f42aa5e3ea919
MuhAminSyam <aminsyammuh@gmail.com>
2025-05-05 11:19:44 +0700

    penggunaan function untuk menunjukkan nama dari class orang yang bisa di akses

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -13,3 +13,18 @@ public:
     void setNama(string pNama);
     friend class siswa; // deklarasi class siswa sebagai teman dari class orang
 };
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a); // metode untuk menunjukkan nama dari class orang
+};
+
+void siswa::setId(int pId)
+{
+    id = pId;
+}
