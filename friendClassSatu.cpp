f0b7e97393c746b3543ae063e120ae296b59a8fc
MuhAminSyam <aminsyammuh@gmail.com>
2025-05-05 11:17:01 +0700

    pemanggilan friend class untuk siswa bisa mengakses class orang

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -5,3 +5,11 @@ using namespace std;
 
 class siswa;
 class orang;
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa; // deklarasi class siswa sebagai teman dari class orang
+};
